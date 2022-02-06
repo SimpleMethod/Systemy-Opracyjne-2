d91397c36d706c8561272ccc7df3cafe950853c3
Michał Młodawski <m.mlodawski@simplemethod.io>
2022-02-06 19:49:04 +0100

    Update InsScript.sh

--- InsScript.sh
+++ InsScript.sh
@@ -2,3 +2,4 @@
 apt-get install -y unzip mc nano curl ssh vsftpd
 curl -l https://raw.githubusercontent.com/SimpleMethod/Systemy-Opracyjne-2/master/vsftpd.conf --create-dirs  -o /etc/vsftpd.conf
 sudo systemctl restart vsftpd 
+echo "Twój adres IP: $(hostname -I)"
