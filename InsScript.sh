97261dcb4aa805c079915fd4348e6421baa71c6e
Michał Młodawski <m.mlodawski@simplemethod.io>
2022-02-06 19:44:31 +0100

    Create InsScript.sh

---
+++ InsScript.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+apt-get install -y unzip mc nano curl ssh vsftpd
+curl -l https://raw.githubusercontent.com/SimpleMethod/Systemy-Opracyjne-2/master/vsftpd.conf --create-dirs  -o /etc/vsftpd.conf
+sudo systemctl restart vsftpd 
